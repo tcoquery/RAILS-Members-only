012aaed38abc32d0e8938e6216fc28e5ec2518e4
tcoquery <tcoquery@gmail.com>
2022-05-12 17:00:43 +0200

    Show the posts and their author when connected on the index page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,27 +2,31 @@ class PostsController < ApplicationController
   before_action :require_login, only: [:new, :create]
 
   def index
+    @post = Post.all.includes(:user)
   end
 
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
-    @post = Post.new(body: params[:body])
+    @post = current_user.posts.new(user_params)
 
     if @post.save
       flash[:success] = "Great! Your post has been created!"
-      redirect_to @post
+      redirect_to posts_url
     else
       flash.now[:error] = "Rats! Fix your mistakes, please."
       render :new, status: :unprocessable_entity
     end
   end
 
-
   private
+
+  def user_params
+    params.require(:post).permit(:body, :user_id)
+  end
 
   def require_login
     unless user_signed_in?
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
 
-  validates :body, :user_id, presence:true
+  validates :body, presence:true
 end
